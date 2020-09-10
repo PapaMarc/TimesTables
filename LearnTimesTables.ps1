1b29bbc4e2975bc8521d48328b663ac6cc32ce60
Seinfeld 神馬Marc <32960421+PapaMarc@users.noreply.github.com>
2020-09-10 13:11:37 +0800

    added verbal response variation

--- LearnTimesTables.ps1
+++ LearnTimesTables.ps1
@@ -5,11 +5,18 @@
 # as i needed an excuse to get my slightly older kiddo to a) practice her times tables, and b) start to code
 # So now she needs to fire up VSCode and powershell for 10min a day... 
 #
-# (also added some suggestions from https://www.reddit.com/user/KyleKowalski/ to spice up the kudos messages a bit)
+
+# Define AttaKiddo and GetBackUpOnTheHorse Arrays
+# used to provide some variation in spoken responses
+$Array_AttaKiddo = @("Great Job!","Congrats","Hurrah","Correct","Booyakasha diggity check this out","Boom","Way to go","High Five","Cheers","Woot","Homerun","wicked","superb","Impressive","Awesome","Great job","Terrific","You get a gold star","Well done","tray bien","dway","muey beuno","exacto")
+$Array_GetBackUpOnTheHorse = @("Doh","Oops","Almost - try again","Not quite","Nope","Thats not it","Nice try buddy","Not so much","No soup for you!","Bummer dude!","If at first you dont succeed try, try again","Nahhh","Bu dway")
+
 
 Add-Type -AssemblyName System.Speech
 $Speech = New-Object System.Speech.Synthesis.SpeechSynthesizer
 $Speech.Speak("Do  you want to play a game? Enter times table to practice, or hit enter for all tables up to 12")
+
+#   Pick your poison
 [Int]$InitTable = Read-Host -Prompt ("Enter times table to practice, or hit enter for all tables up to 12")
 $Table = $InitTable
 
@@ -26,21 +33,25 @@ while($true) {
             if ($Product -eq $Answer) {
                 Write-host "Correct" -ForegroundColor "Green"
                 Write-host "---"
-                $Speech.Speak("Correct! You entered $Answer. $Table times $Factor equals $Product.")
+                $Recognition = $Array_AttaKiddo | Get-Random
+                $Speech.Speak("$Recognition. You entered $Answer. $Table times $Factor equals $Product.")
             }
-    # '^\d+$' == regex for 'only numbers'
+    #      '^\d+$' == regex for 'only numbers'
            elseif ($Answer -NotMatch '^\d+$') {
                Write-Host "The correct answer is $Product"
                Write-host "---"
                $Speech.Speak("That was not a number. The correct answer is $Product.")
-           } 
+            } 
             elseif ($Product -ne $Answer) {
                 Write-host "Incorrect" -ForegroundColor "Red"
                 Write-host "The correct answer is $Product"
                 Write-host "---"
-                $Speech.Speak("Incorrect. You should have entered ;$Product, but you entered ;$Answer. $Table times $Factor equals $Product.")
+    #           Get a GetBackUpOnTheHorse (alternate way to get random element from the array v. AttaKiddo above)
+                $Recognition = Get-Random -InputObject $Array_GetBackUpOnTheHorse
+                $Speech.Speak("$Recognition. You should have entered ;$Product, but you entered ;$Answer. $Table times $Factor equals $Product.")
             }
-        } else {
-            $Speech.Speak("You did not provide an answer.")
         }
+        else {
+            $Speech.Speak("You did not provide an answer.")
+    }
 }
--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 Practice times tables
 and play with VSCode, GitHub and a little code... 
 
-inspired by, and built upon:
+inspired by, and built upon the wonderful foundation provided by:
 # https://www.reddit.com/r/PowerShell/comments/ikoy4c/how_i_got_my_6_year_old_to_practice_the_alphabet/
 by u/_lahell_  https://www.reddit.com/user/_lahell_/
 
-Not as concise and elegant as the ABCs original...
-It's hard to stay lean while giving into some feature creep. Good lesson in itself. 
-I needed an excuse to get my slightly older kiddo to a) practice times tables, and b) start to code
-So now she fires up VSCode and PowerShell for 10min a day... 
+LearnTimesTables.ps1 is NOT as concise and elegant as the ABCs original...
+It's hard to stay lean while giving into some feature creep. Great lesson in itself. 
+That said, I needed an excuse to get my slightly older kiddo to a) practice times tables, and b) start to code
+So a few tweaks from me now facilitate her firing up VSCode and PowerShell for 10min a day...
 
-Next turn of the crank will be a bit more verbose, though perhaps a bit more fun with variation in accolades
+An additinal turn of the crank is a bit more verbose, though perhaps will hold attention for a couple extra rounds with with variation in accolades
 as suggested by u/_lahell_  https://www.reddit.com/user/_lahell_/
 
